b2a035584c63101e5f3a76e54b546f3f75f7f425
Donovan Galloway <genericsalesman6@gmail.com>
2022-11-27 20:54:50 -0800

    checkpoint added some styling

--- react-app/src/components/Posts/index.js
+++ react-app/src/components/Posts/index.js
@@ -1,6 +1,7 @@
 import {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import { getAllUserTransactions } from '../../store/transactions'
+import './Posts.css'
 
 function Posts () {
     const dispatch = useDispatch()
@@ -14,12 +15,24 @@ function Posts () {
     
     if (!posts) return <h1>Loading...</h1>
     return (
-        <div>
+        <div className='posts-container'>
+            
             {posts && !!posts.length && posts.filter((post) => post.is_Pending == false && post.transaction_state == 'approved').map((post,index) => {
 
                 return(
-                    <div key={index}>
-                        <p>{post.sender_id} sent ${post.request_amount} to {post.receiver_id}</p>
+                    <div key={index} className='individual-post-container'>
+                        <div className='user-avatar-container'>
+                            <button className='avatar-button'>DG</button>
+                        </div>
+                        
+                        <div className='post-user-info-container'>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                            <p><strong>{post.sender_id}</strong> paid ${post.request_amount} to <strong>{post.receiver_id}</strong></p>   
+                        
+                    </div>
                         
                     </div>
                 )
