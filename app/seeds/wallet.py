d11efce1a6c3fbfbed9704323ab0e90c8d3a720b
Donovan Galloway <genericsalesman6@gmail.com>
2022-12-03 19:43:08 -0800

    fix seed data

--- app/seeds/wallet.py
+++ app/seeds/wallet.py
@@ -4,9 +4,11 @@ from app.models import db, Wallet, environment, SCHEMA
 def seed_wallets():
     demo_wallet = Wallet(user_id = 1, balance = 10000)
     test_wallet = Wallet(user_id = 2, balance = 5000)
+    newb_wallet = Wallet(user_id = 3, balance = 1000)
 
     db.session.add(demo_wallet)
     db.session.add(test_wallet)
+    db.session.add(newb_wallet)
     db.session.commit()
 
 
